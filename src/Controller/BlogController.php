3c6aab5b3ef4ec8877ccba98335b3bc78a4614ee
Russkazkin <russkazkin@gmail.com>
2022-11-19 15:35:09 +0300

    Add methods for BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/blog')]
@@ -23,14 +25,27 @@ class BlogController extends AbstractController
         return $this->render('blot/index.html.twig', ['posts' => $this->session->get('posts')]);
     }
 
+    /**
+     * @throws Exception
+     */
     #[Route('/add', name: 'blog_add')]
-    public function add()
+    public function add(): void
     {
-
+        $posts = $this->session->get('posts');
+        $posts[uniqid('post', true)] = [
+            'title' => 'A random title ' . random_int(1, 500),
+            'text' => 'Some random text nr ' . random_int(1, 500),
+        ];
+        $this->session->set('posts', $posts);
     }
-    #[Route('/show/{id}', name: 'blog_show')]
-    public function show($id)
-    {
 
+    #[Route('/show/{id}', name: 'blog_show')]
+    public function show($id): Response
+    {
+        $posts = $this->session->get('posts');
+        if (!$posts || !isset($posts[$id])) {
+            throw new NotFoundHttpException('Post not found');
+        }
+        return $this->render('blog/post.html.twig', ['id' => $id, 'post' => $posts[$id]]);
     }
 }
