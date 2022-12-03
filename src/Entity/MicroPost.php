7970c28f15f42f9af765f001247a500c91efc609
Russkazkin <russkazkin@gmail.com>
2022-12-03 06:53:42 +0300

    Add first entity

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MicroPostRepository;
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MicroPostRepository::class)]
+class MicroPost
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 280)]
+    private ?string $text = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?DateTimeInterface $time = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getTime(): ?DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+}
