2d9b715bfeb804b1093343ddb82dac9db8eeca43
Russkazkin <russkazkin@gmail.com>
2023-02-25 15:31:55 +0300

    Add micro post  form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 #[Route('/micro-post')]
 class MicroPostController extends AbstractController
@@ -20,8 +28,20 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/add', 'micro_post_add')]
-    public function add()
+    public function add(Request $request, FormFactoryInterface $formFactory, EntityManagerInterface $entityManager, RouterInterface $router): Response
     {
-        return $this->render('micro_post/add.html.twig');
+        $microPost = new MicroPost();
+        $microPost->setTime(new DateTime());
+
+        $form = $formFactory->create(MicroPostType::class, $microPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($microPost);
+            $entityManager->flush();
+            return new RedirectResponse($router->generate('micro_post_index'));
+        }
+
+        return $this->render('micro_post/add.html.twig', ['form' => $form->createView()]);
     }
 }
