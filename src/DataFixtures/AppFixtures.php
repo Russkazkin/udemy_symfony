13cc1e9f5fa143ee31f712cd011e42a88d893d85
Russkazkin <russkazkin@gmail.com>
2023-02-25 07:01:25 +0300

    Add fixture for micro post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\MicroPost;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Exception;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @throws Exception
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for ($i = 0; $i < 10; $i++) {
+            $microPost = new MicroPost();
+            $microPost->setText('Some random text ' . random_int(0, 100));
+            $microPost->setTime(new DateTime());
+            $manager->persist($microPost);
+        }
         $manager->flush();
     }
 }
